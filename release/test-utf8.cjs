9db83f4e202bb9d4d8f856275c4495c535d3f572
Bassam Alsaqqa <Bassamalsaqqa@hotmail.com>
2026-09-08 21:59:37 +0300

    revert HostPapa deployment test

--- release/test-utf8.cjs
+++
@@ -1,8 +0,0 @@
-﻿const fs = require("fs");
-
-const s = fs.readFileSync("Arabic.json", "utf8");
-
-console.log(s.includes("صوت الميلاد") ? "OK: Arabic present" : "FAIL");
-
-const m = s.match(/"title":\s*"([^"]+)"/);
-console.log(m ? m[1] : "NO MATCH");
